d9fc335acf7a6750533a6981baf8ebc35aab5649
Masamine Someha <msomeha@gmail.com>
2023-11-25 17:03:11 -0800

    Added the relevance statistics.

--- analyze_news_data.py
+++ analyze_news_data.py
@@ -7,6 +7,10 @@ months = pd.date_range(start='6/1/2022',
 key = 'COST'
 sum = 0
 numDates = 0
+
+# Counting how many are actually relevant.
+totalCount = 0
+relevantCount = 0
 for m in months:
     month = m.strftime('%Y-%m')
     fileName = '{}_{}_NEWS.csv'.format(key, month) 
@@ -21,7 +25,7 @@ for m in months:
     dateStr = dateDateTime.strftime(dateFormat)
     count = 0
     for index, row in df.iterrows():
-
+        
         dateDateTime = datetime.strptime(row['time_published'], inputFormat)
 
         if (dateStr == dateDateTime.strftime(dateFormat)):
@@ -32,9 +36,17 @@ for m in months:
             count = 1
             dateStr = dateDateTime.strftime(dateFormat)
             # print(dateStr)
+        
+        totalCount = totalCount + 1
+        if (row['relevance_score'] > 0.8):
+            relevantCount = relevantCount + 1
+
+
     
     sum = sum + count
     numDates = numDates + 1
 
 avg = sum/numDates
-print('Average number of {} news per day'.format(avg))
+relevantPct = relevantCount/totalCount*100
+print('Average number of {:.2f} news per day'.format(avg))
+print('Out of {} news {} ({:.2f}%) were relevant news'.format(totalCount, relevantCount, relevantPct))
